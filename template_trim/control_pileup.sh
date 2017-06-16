cd4118fba625ed701de73ac823480ed230880f3a
HamLam <lamx0031@umn.edu>
2017-06-16 11:40:34 -0500

    Update control_pileup.sh

--- template_trim/control_pileup.sh
+++ template_trim/control_pileup.sh
@@ -21,8 +21,8 @@ S_DB=seq_db
 #ref=/panfs/roc/rissdb/genomes/Homo_sapiens/hg19_canonical/seq/hg19_canonical.fa
 ref=${S_DB}
 
-bwacommand="bwa mem -M -t 24 $BWA_DB $c_S1_R1 $c_S1_R2 | samtools view -q 10 -bS - > c_bwa.bam"
-btcommand="bowtie2 -p 24 -k 5 -x $BOWTIE2_DB -1 $c_S1_R1 -2 $c_S1_R2 | samtools view -q 10 -bS - > c_bowtie2.bam"
+bwacommand="bwa mem -M -t 16 $BWA_DB $c_S1_R1 $c_S1_R2 | samtools view -q 10 -bS - > c_bwa.bam"
+btcommand="bowtie2 -p 16 -k 5 -x $BOWTIE2_DB -1 $c_S1_R1 -2 $c_S1_R2 | samtools view -q 10 -bS - > c_bowtie2.bam"
 
 #echo ${bwacommand} > $WORKING_PATH/aligncommands
 #echo ${btcommand} >> $WORKING_PATH/aligncommands
