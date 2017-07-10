446383c7ab417f4a3ddca01615c61c55aed11085
HamLam <lamx0031@umn.edu>
2017-07-10 16:09:42 -0500

    Update run_sample1.sh

--- template_trim/run_sample1.sh
+++ template_trim/run_sample1.sh
@@ -160,7 +160,7 @@ grep "mysqlimport" $working_dir/completed.txt > /dev/null 2>&1
 else
  echo "Run mysqlimport"
 
-     mysqlimport --local --socket=$BASE/thesock -u root cnv1 --use-threads=24 \
+     mysqlimport --local --socket=$BASE/thesock -u root cnv1 --use-threads=8 \
      cnv_control_name_bwa_pileup_no_dup.chr1_t \
      cnv_control_name_bwa_pileup_no_dup.chr2_t \
      cnv_control_name_bwa_pileup_no_dup.chr6_t \
