28c8b32e66b12967e580b7da2507e093e5c1e32a
HamLam <lamx0031@umn.edu>
2017-07-24 13:07:18 -0500

    Update run_sample7.sh

--- template_trim/run_sample7.sh
+++ template_trim/run_sample7.sh
@@ -160,7 +160,7 @@ grep "mysqlimport" $working_dir/completed.txt > /dev/null 2>&1
 else
  echo "Run mysqlimport"
 
-     mysqlimport --local --socket=$BASE/thesock -u root cnv7 --use-threads=8 \
+     mysqlimport --local --socket=$BASE/thesock -u root cnv7 --use-threads=5 \
      cnv_control_name_bwa_pileup_no_dup.chr21_t \
      cnv_control_name_bwa_pileup_no_dup.chr22_t \
      cnv_control_name_bwa_pileup_no_dup.chrX_t \
@@ -236,6 +236,7 @@ else
 
   if [[ $? -ne 0 ]]; then
    echo "Run mysqlimport failed" >&2
+   echo "g7 Run mysqlimport failed"
        ## mysqladmin --socket=$BASE/thesock shutdown -u root
         exit 1
      else
