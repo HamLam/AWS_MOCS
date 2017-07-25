7368790e5c6aaf1ff5ed60e0e3aafa01059e76bb
HamLam <lamx0031@umn.edu>
2017-07-25 10:40:03 -0500

    Update run_sample6_wrapper.sh

--- template_trim/run_sample6_wrapper.sh
+++ template_trim/run_sample6_wrapper.sh
@@ -21,7 +21,7 @@ else
 fi
 
 ATTEMPTS=1
- while [ $ATTEMPTS -lt 20 ]; do
+ while [ $ATTEMPTS -lt 10 ]; do
     echo -e "\n\n------------- ATTEMPT $ATTEMPTS ---------------\n\n"
       sh ${working_dir_g6}/run_cnv_${g6}.sh 
    ec=$?
